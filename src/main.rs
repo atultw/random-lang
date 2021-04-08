a8406014f12f80225f3034959d43af3e2f3514e0
Atulya <a>
2021-04-07 21:06:19 -0700

    interpret properly as a tree, PEMDAS to come later but for now proceeds in the order written

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+// use std::num::ParseIntError;
+
+// use core::panicking::{panic_fmt, panic_str};
+
+use std::process::exit;
+use std::thread::current;
 
 fn main() {
-    let program = "1 + 2 + 10";
+    let program = "2 * 5 - 10 + 4 / 2";
 
     compile(program.parse().unwrap())
 }
 
+#[derive(Debug)]
 enum Val {
     Given(u8),
     Expression(Box<Expr>),
@@ -19,6 +26,7 @@ impl Val {
     }
 }
 
+#[derive(Debug)]
 struct Expr {
     object: Val,
     operator: String,
@@ -39,10 +47,34 @@ impl Expr {
 }
 
 fn compile(input: String) {
-    let stuff: Vec<&str> = input.split(" ").collect();
-    // println!("{:?}", stuff);
+    let reserved: Vec<&str> = vec!["+", "-", "*", "/", "=", "!", "|"];
 
-    println!("{:?}", stuff.iter().position(|&r| r == "+").unwrap());
+    let stuff: Vec<&str> = input.split(" ").collect();
+
+    let mut i = 0;
+    while i < stuff.len() {
+        if i % 2 == 0 && reserved.contains(&stuff[i]) {
+            println!("Reserved Word {:?} at pos {:?}", stuff[i], i);
+            exit(0);
+        }
+
+        if i % 2 == 0 {
+            match stuff[i].parse::<u8>() {
+                Ok(_) => { println!("Number found"); }
+                Err(_) => {
+                    println!("Non-integer operand {:?} at pos {:?}", stuff[i], i);
+                    exit(0);
+                }
+                // _ => {}
+            };
+        }
+
+        if i % 2 == 1 && !reserved.contains(&stuff[i]) {
+            println!("Word {:?} at pos {:?} is not an operator", stuff[i], i);
+            exit(0);
+        }
+        i = i + 1;
+    }
 
     let first = Expr {
         object: Val::Given(String::from(stuff[0]).parse().unwrap()),
@@ -50,13 +82,25 @@ fn compile(input: String) {
         subject: stuff[2].parse().unwrap(),
     };
 
-    let second = Expr {
-        object: Val::Expression(Box::from(first)),
-        operator: stuff[3].parse().unwrap(),
-        subject: stuff[4].parse().unwrap()
-    };
+    // let mut i = 0;
+    fn to_tree(all: Vec<&str>, prev: Expr, index: usize) -> u8 {
+        if index >= all.len() as usize {
+            // done
+            return prev.eval()
+        }
 
-    let res = Box::from(second.eval());
+        let current = Expr {
+            object: Val::Expression(Box::from(prev)),
+            operator: all[index].parse().unwrap(),
+            subject: all[index+1].parse().unwrap(),
+        };
+        println!("{:?}", current);
+        to_tree(all, current, index+2)
+    }
+
+    let res= to_tree(stuff, first, 3);
+
+    // let res = Box::from(second.eval());
     println!("{:?}", res);
     return;
 }
